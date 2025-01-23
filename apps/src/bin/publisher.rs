4b109100cae3e752fef44f2a7454b58484ee25be
ttdung <ttdung@gmail.com>
2025-01-23 10:10:57 +0700

    fix journal format

--- apps/src/bin/publisher.rs
+++ apps/src/bin/publisher.rs
@@ -30,6 +30,7 @@ use url::Url;
 use std::fs::File;
 use std::io::Read;
 use std::env;
+use std::io::Write;
 // `IEvenNumber` interface automatically generated via the alloy `sol!` macro.
 alloy::sol!(
     #[sol(rpc, all_derives)]
@@ -99,19 +100,26 @@ fn main() -> Result<()> {
 
     println!(" finish create proof....");
 
+        // Dump receipe using serde
+    let receipt_json = serde_json::to_string_pretty(&receipt).unwrap();
+        
+    // Write the JSON string to a file
+    let mut file = File::create("./receipt.json").expect("failed to create file");
+    file.write_all(receipt_json.as_bytes()).expect("failed to write");
+
     // Encode the seal with the selector.
     let seal = encode_seal(&receipt)?;
 
     // Extract the journal from the receipt.
     let journal = receipt.journal.bytes.clone();
-    
+
     let flag = receipt.verify(IS_EVEN_ID).unwrap();
     println!("Flag {:?}", flag);
 
     // Decode Journal: Upon receiving the proof, the application decodes the journal to extract
     // the verified number. This ensures that the number being submitted to the blockchain matches
     // the number that was verified off-chain.
-    let x = String::abi_decode(&journal, true).context("decoding journal data")?;
+    let x = Vec::<u8>::abi_decode(&journal, true).context("decoding journal data")?;
 
     // // Construct function call: Using the IEvenNumber interface, the application constructs
     // // the ABI-encoded function call for the set function of the EvenNumber contract.
@@ -131,9 +139,11 @@ fn main() -> Result<()> {
     let seal_hex_string = vec_to_hex_string(&seal);
     println!("seal hex_string: {}", seal_hex_string);
 
-    let x_hex_string = hex::encode(x);
+    // let x_hex_string = hex::encode(x);
+
+    let x_hex_string = vec_to_hex_string(&x);
     
-    println!("x: 0x{}", x_hex_string);
+    println!("x: {}", x_hex_string);
 
     Ok(())
 }
